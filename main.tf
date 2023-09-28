384c013641c4ed4c944329d9f3a1e431f3bae410
Ram Teja <vasupariramteja123@gmail.com>
2023-09-28 13:00:43 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,25 @@
+provider "aws" {
+  region = var.location
+}
+
+data "terraform_remote_state" "remote_state" {
+  backend = "s3"
+  config = {
+    bucket         = "s3statebackend1109"
+    key            = "global/mystatefile/terraform.tfstate"
+    region         = "us-east-1"  # Replace with your bucket's region
+    encrypt        = true
+    
+  }
+}
+
+
 resource "aws_instance" "demo-server" {
   count = var.instance_count
   ami=var.os-name
   key_name=var.key
   instance_type=var.instance-type
-  associate_public_ip_address = true
-  subnet_id = aws_subnet.demo-subnet-1.id
-  vpc_security_group_ids = [ aws_security_group.demo-vpc-sg.id ]
+  # associate_public_ip_address = true
+  subnet_id = data.terraform_remote_state.remote_state.Private_subnet_id
+  vpc_security_group_ids = [ data.terraform_remote_state.remote_state.security_group_id ]
 }
