94079623e583a1d586baf0be2929d4aa16c14f2d
Ram Teja <vasupariramteja123@gmail.com>
2023-09-28 13:05:22 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -3,3 +3,16 @@ variable "instance_count" {
   type        = number
   default     = 3  # Set a default value or change as needed
 }
+
+
+variable "os-name" {
+  default = "ami-053b0d53c279acc90"
+}
+
+variable "instance-type" {
+  default = "t2.small"
+}
+
+variable "key" {
+  default = "ram-master-key"
+}
